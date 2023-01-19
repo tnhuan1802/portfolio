d26fd64a9f1781b2b7563e67fb4e5dd0a0ad6b6c
unknown <tnhuan1802>
2023-01-19 14:39:36 +0700

    Create multiple sections for one screen

---
+++ src/components/MainPage/About.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './About.css'
+
+interface IAboutProps {
+}
+
+const About: React.FC<IAboutProps> = (props) => {
+  return (
+    <div className='about-container'>Hello</div>
+  )
+};
+
+export default About;
